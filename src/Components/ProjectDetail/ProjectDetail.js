3995f92d222ca70de4cac65415c8be4a36d293cc
Joseph Grounds <primemtn1@gmail.com>
2019-05-09 14:32:20 -0600

    styling fix

--- src/Components/ProjectDetail/ProjectDetail.js
+++ src/Components/ProjectDetail/ProjectDetail.js
@@ -30,63 +30,61 @@ class ProjectDetail extends Component {
         if(!projects){
             return <h1>...Loading</h1>
         }
-        else{
-            return(
-                this.props.info.map(item => {
-                    if(projectId===item.id){
-                        return(
-                            <div key={item.id}>
-                                <div className="container-fluid ">
-                                    <div className="row">
-                                        <div className="col-lg-12 noPadding">
-                                            <img src={item.image} style={{backgroundImage: "url(" + item.image + ")", height:'70vh', width: '100%'}} className="text-center" alt={item.title}/>
-                                        </div>
-                                        <div className="col-lg-12 title littlePadding">
-                                            <h1>{item.title}</h1>
-                                        </div>
+        return(
+            this.props.info.map(item => {
+                if(projectId===item.id){
+                    return(
+                        <div key={item.id}>
+                            <div className="container-fluid ">
+                                <div className="row">
+                                    <div className="col-lg-12 noPadding">
+                                        <img src={item.image} style={{backgroundImage: "url(" + item.image + ")", height:'70vh', width: '100%'}} className="text-center" alt={item.title}/>
+                                    </div>
+                                    <div className="col-lg-12 title littlePadding">
+                                        <h1>{item.title}</h1>
                                     </div>
                                 </div>
-                                <div className="container-fluid ">
-                                    <div className="row">
-                                        <div className="col-lg-6 description">
-                                            <h2 style={{color:'#fff',}}>Description</h2>
-                                            <h3 style={{color:'#fff',}}>{item.description}</h3>
-                                        </div>
-                                        <div className="col-lg-6 bullets">
-                                            <h2 style={{color:'#585858',}}>Tech Used</h2>
-                                            <h3>{item.tech}</h3>
-                                        </div>    
-                                    </div>
-                                </div>
-                                <div className="container-fluid">
-                                    <div className="row">
-                                        <div className="col-lg-6 gitHubBack">
-                                            <h2>Github</h2>
-                                            <a target='blank' href={item.url}>
-                                                <i className="fab fa-github-square"></i>
-                                            </a>
-                                        </div>
-                                        <div className="col-lg-6 descriptionTwo"
-                                        style={{backgroundImage: "url(" + item.image + ")"}}
-                                        >
-                                        </div>    
-                                    </div>
-                                </div>
-                                <div className="container-fluid ">
-                                    <div className="row">
-                                        <div className="col-lg-12 noPadding">
-                                            <img src={item.image_three} style={{backgroundImage: "url(" + item.image + ")", height:'70vh', width: '100%'}} className="text-center" alt={item.title}/>
-                                        </div>
-                                    </div>
-                                </div>
-                                <Footer/>
                             </div>
-                            
-                        ) 
-                    }
-                })  
-            ) 
-        }
+                            <div className="container-fluid ">
+                                <div className="row">
+                                    <div className="col-lg-6 description">
+                                        <h2 style={{color:'#fff',}}>Description</h2>
+                                        <h3 style={{color:'#fff',}}>{item.description}</h3>
+                                    </div>
+                                    <div className="col-lg-6 bullets">
+                                        <h2 style={{color:'#585858',}}>Tech Used</h2>
+                                        <h3>{item.tech}</h3>
+                                    </div>    
+                                </div>
+                            </div>
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <div className="col-lg-6 gitHubBack">
+                                        <h2>Github</h2>
+                                        <a target='blank' href={item.url}>
+                                            <i className="fab fa-github-square"></i>
+                                        </a>
+                                    </div>
+                                    <div className="col-lg-6 descriptionTwo"
+                                    style={{backgroundImage: "url(" + item.image + ")"}}
+                                    >
+                                    </div>    
+                                </div>
+                            </div>
+                            <div className="container-fluid ">
+                                <div className="row">
+                                    <div className="col-lg-12 noPadding">
+                                        <img src={item.image_three} style={{backgroundImage: "url(" + item.image + ")", height:'70vh', width: '100%'}} className="text-center" alt={item.title}/>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* <Footer/> */}
+                        </div>
+                    ) 
+                }
+            })  
+        ) 
+        
 
     }
     render() {
